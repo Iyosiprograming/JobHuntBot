cb56ff023ec10b4980c48cab411adb42da00d48e
Iyosiyas <iyosieyosiyas@gmail.com>
2025-12-26 18:15:12 +0300

    feat: add server setup and MongoDB connection logic

---
+++ server.js
@@ -0,0 +1,18 @@
+import express from 'express';
+import connectDB from './config/connectDB.js';
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+const startServer = async () => {
+    try {
+        await connectDB();
+        app.listen(PORT, () => {
+            console.log(`Server is running on http://localhost:${PORT}`);
+        });
+    } catch (error) {
+        console.error("Error starting server:", error);
+        }
+    };
+
+startServer();
