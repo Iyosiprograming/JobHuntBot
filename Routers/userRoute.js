b26a5f0f2133c9425a8049b34b737d2a8ce27fbd
Iyosiyas <iyosieyosiyas@gmail.com>
2025-12-26 18:53:38 +0300

    feat: add user routing for creating new users

---
+++ Routers/userRoute.js
@@ -0,0 +1,8 @@
+import {createUser} from '../Controllers/userController.js';
+import express from 'express';
+
+const router = express.Router();
+
+router.post('/create', createUser);
+
+export default router;
--- server.js
+++ server.js
@@ -1,15 +1,20 @@
 import express from 'express';
 import connectDB from './config/connectDB.js';
+import userRouter from './Routers/userRoute.js';
+
+
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+app.use(express.json());
 const startServer = async () => {
     try {
         await connectDB();
         app.listen(PORT, () => {
             console.log(`Server is running on http://localhost:${PORT}`);
         });
+        app.use('/api/users', userRouter);
     } catch (error) {
         console.error("Error starting server:", error);
         }
